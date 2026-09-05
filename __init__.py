1d53212c4035b960f21f7e79efd62298d136db80
Ayeeb <nangsin1990@gmail.com>
2026-09-05 19:46:01 +0700

    Delete __init__.py

--- __init__.py
+++
@@ -1,7 +0,0 @@
-# Package marker for dashboard imports.
-try:
-    from .config import REVISION, REVISION_TAG
-except ImportError:
-    from config import REVISION, REVISION_TAG
-
-__all__ = ["REVISION", "REVISION_TAG"]
